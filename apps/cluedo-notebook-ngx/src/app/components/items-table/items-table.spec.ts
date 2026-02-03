8a38cc56ed2e85527d6ea16c26875ea661897025
Adam Wolak <adamwolak0501@gmail.com>
2026-02-03 07:53:48 +0100

    CI: Modify component for running tests.

--- apps/cluedo-notebook-ngx/src/app/components/items-table/items-table.spec.ts
+++ apps/cluedo-notebook-ngx/src/app/components/items-table/items-table.spec.ts
@@ -1,13 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemsTable } from './items-table';
+import {Signal, signal} from "@angular/core";
+import {Assignments, CluedoState, Player} from "../../+state/state.model";
+import {cluedoStore} from "../../+state/store";
+
+type CluedoSignalState = {
+  [K in keyof CluedoState]: Signal<CluedoState[K]>
+} & {
+  // computed
+  canAddMorePlayer: Signal<boolean>,
+  playersInGame: Signal<Player[]>
+};
 
 describe('ItemsTable', () => {
   let component: ItemsTable;
   let fixture: ComponentFixture<ItemsTable>;
+  let mockedState: CluedoSignalState;
+
+  mockedState = {
+    gameDefinition: signal<string>('classic'),
+    players: signal<Player[]>([]),
+    suspects: signal<string[]>([]),
+    rooms: signal<string[]>([]),
+    items: signal<string[]>([]),
+    assignments: signal<Assignments>({}),
+    canAddMorePlayer: signal<boolean>(true),
+    playersInGame: signal<Player[]>([])
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ItemsTable],
+      providers: [
+        { provide: cluedoStore, useValue: mockedState }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ItemsTable);
@@ -18,4 +44,6 @@ describe('ItemsTable', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+   // TODO Add more tests
 });
