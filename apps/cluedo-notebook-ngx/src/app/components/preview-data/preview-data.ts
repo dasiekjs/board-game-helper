5b4356938d41db57472a96c2274ab1162d511198
Adam Wolak <adamwolak0501@gmail.com>
2026-02-05 21:35:32 +0100

    Feat: Selection preview - add close button.

--- apps/cluedo-notebook-ngx/src/app/components/preview-data/preview-data.ts
+++ apps/cluedo-notebook-ngx/src/app/components/preview-data/preview-data.ts
@@ -2,18 +2,22 @@ import {Component, computed, inject} from '@angular/core';
 import { cluedoStore } from "../../+state/store";
 import {TranslocoPipe} from "@jsverse/transloco";
 import {CircleQuestionMarkIcon, DotIcon, LucideAngularModule, XIcon} from "lucide-angular";
+import {MatIconButton} from "@angular/material/button";
+import {MatBottomSheetRef} from "@angular/material/bottom-sheet";
 
 @Component({
   selector: 'clu-preview-data',
   imports: [
     TranslocoPipe,
-    LucideAngularModule
+    LucideAngularModule,
+    MatIconButton
   ],
   templateUrl: './preview-data.html',
   styleUrl: './preview-data.css',
 })
 export class PreviewData {
   private readonly cluedoStore = inject(cluedoStore);
+  private readonly bottomSheetRef = inject(MatBottomSheetRef);
 
   private summary = () => this.cluedoStore.summary();
 
@@ -26,4 +30,8 @@ export class PreviewData {
   protected readonly questionMarkIcon = CircleQuestionMarkIcon;
   protected readonly dotIcon = DotIcon;
   protected readonly xIcon = XIcon;
+
+  protected close() {
+    this.bottomSheetRef.dismiss();
+  }
 }
