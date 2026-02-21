00a081668f09cd818c23edcc246bfba016f1937d
Adam Wolak <adamwolak0501@gmail.com>
2026-02-21 21:36:23 +0100

    WIP: Dream

---
+++ apps/dreams-dashboard-ngx/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Route } from '@angular/router';
+import {SelectPlayers} from "./pages/select-players/select-players";
+import {GameDashboard} from "./pages/game-dashboard/game-dashboard";
+
+export const appRoutes: Route[] = [
+  {
+    path: 'init-game',
+    component: SelectPlayers,
+  },
+  {
+    path: 'dashboard',
+    component: GameDashboard,
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
